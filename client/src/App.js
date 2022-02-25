27c43c2899d3c1ce3847d1f9114f1603b66a6546
Dok <doslovdok@gmail.com>
2022-02-25 13:16:06 +0100

    removed pages nav

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,6 @@ import { AllJobs, Profile, SharedLayout, Stats, AddJob } from "./pages/dashboard
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to='/'>Dashboard</Link>
-        <Link to='/register'>Register</Link>
-        <Link to='/landing'>Landing</Link>
-      </nav>
       <Routes>
         <Route
           path='/'
